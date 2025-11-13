fc4ac6cc99f63ea8da88aa27e4ab55b41374b416
DESKTOP-0DD7SIJ\Alejandro M <alejandromontesinospozo@gmail.com>
2025-11-13 09:59:02 +0100

    correccion tema oscuro

--- project-root/public/js/main.js
+++ project-root/public/js/main.js
@@ -1,13 +1,18 @@
-document.addEventListener ( 'DOMContentLoaded' , () => {
- const interruptor = document.getElementById ( 'theme-toggle' ) ;
- const darkModeEnabled = localStorage.getItem ( 'dark-mode' ) === 'true' ;
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleSwitch = document.getElementById('theme-toggle');
+    const darkModeEnabled = localStorage.getItem('dark-mode') === 'true';
 
- document.body.classList.toggle ( 'dark-mode' , darkModeEnabled ) ;
- interruptor.checked = modooscuroActivado ;
+    document.body.classList.toggle('dark-mode', darkModeEnabled);
+    toggleSwitch.checked = darkModeEnabled;
 
- toggleSwitch.addEventListener ( 'change' , () => {
- const isDark = toggleSwitch.checked ;
- document.body.classList.toggle ( 'dark-mode' , isDark ) ;
- localStorage.setItem ( 'modo oscuro' , isDark );
- });
- });
+    toggleSwitch.addEventListener('change', () => {
+        const isDark = toggleSwitch.checked;
+        document.body.classList.toggle('dark-mode', isDark);
+
+        if (isDark) {
+            localStorage.setItem('dark-mode', 'true');
+        } else {
+            localStorage.removeItem('dark-mode');
+        }
+    });
+});
