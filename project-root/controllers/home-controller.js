bf1a1f589e0e44be337088ba49a3d627120ac246
DESKTOP-2IK9HPO\IreneOD <ireneod20@gmail.com>
2025-11-14 12:30:47 +0100

    Subida: se muestra el home.html, solo si hay un usuario logueado, en caso de que se intente acceder sin estar logueado se redirige al landing.html

--- project-root/controllers/home-controller.js
+++ project-root/controllers/home-controller.js
@@ -1,9 +1,28 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
 
-document.getElementById("btn_users").addEventListener("click", function() {
+  document.getElementById("btn_users").addEventListener("click", function () {
     window.location.href = "users.html";
   });
 
+  const currentUser = sessionStorage.getItem('currentUser');
+
+  if (!currentUser && window.location.pathname !== '/landing.html') {
+    // Redirigir si el usuario no está logueado
+    window.location.href = 'landing.html';
+    return;
+  }
+
+  if (currentUser) {
+    const ul = document.getElementById("list");
+
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = 'home.html';
+    a.textContent = 'Explorar Actividades';
+
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
 
 
 });
---
+++ project-root/controllers/landing-controller.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const currentUser = sessionStorage.getItem('currentUser');
+
+    if (currentUser) {
+        const ul = document.getElementById("list");
+
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = 'home.html';
+        a.textContent = 'Explorar Actividades';
+
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+});
